5e7cfaf246c03f1f44d7c73697c0117b9e15be78
devansh01sep <devanshofficial0109@gmail.com>
2026-06-08 13:46:39 +0530

    Valid Anagram

---
+++ Week_2/Day_1/ValidAnagram.java
@@ -0,0 +1,21 @@
+package Week_2.Day_1;
+
+import java.util.Arrays;
+
+public class ValidAnagram {
+
+    public boolean isAnagram(String s, String t) {
+        int[] count = new int[26];
+
+        for (char ch : s.toCharArray()) {
+            count[ch - 'a']++;
+        }
+
+        for (char ch : t.toCharArray()) {
+            count[ch - 'a']--;
+        }
+
+        boolean allZeros = Arrays.stream(count).allMatch(element -> element == 0);
+        return allZeros;
+    }
+}
