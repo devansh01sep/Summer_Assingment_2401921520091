b3960ee1a68566ab5d68d106094b480c3c957cca
devansh01sep <devanshofficial0109@gmail.com>
2026-06-16 11:20:54 +0530

    Implement Test class for Playable interface

---
+++ Week_2/OOPS/Music/String/Veena.java
@@ -0,0 +1,11 @@
+package Week_2.OOPS.Music.String;
+
+import Week_2.OOPS.Music.Playable;
+
+public class Veena implements Playable {
+
+    @Override
+    public void play() {
+        System.out.println("Playing Veena");
+    }
+}
---
+++ Week_2/OOPS/Music/Wind/Saxophone.java
@@ -0,0 +1,11 @@
+package Week_2.OOPS.Music.Wind;
+
+import Week_2.OOPS.Music.Playable;
+
+public class Saxophone implements Playable {
+
+    @Override
+    public void play() {
+        System.out.println("Playing Saxophone");
+    }
+}
