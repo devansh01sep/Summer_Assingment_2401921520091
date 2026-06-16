ab9bbc3b84e3766e2322456675d9e997b0c004bd
devansh01sep <devanshofficial0109@gmail.com>
2026-06-16 10:57:15 +0530

    Generate Parentheses

---
+++ Week_2/Day_4/GenerateParentheses.java
@@ -0,0 +1,28 @@
+package Week_2.Day_4;
+
+import java.util.*;
+
+public class GenerateParentheses {
+
+    private List<String> result = new ArrayList<>();
+
+    public List<String> generateParenthesis(int n) {
+        solve(n, "", 0, 0);
+        return result;
+    }
+
+    private void solve(int n, String curr, int open, int close) {
+        if (curr.length() == 2 * n) {
+            result.add(curr);
+            return;
+        }
+
+        if (open < n) {
+            solve(n, curr + "(", open + 1, close);
+        }
+
+        if (close < open) {
+            solve(n, curr + ")", open, close + 1);
+        }
+    }
+}
