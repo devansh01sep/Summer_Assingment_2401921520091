fe52bf747706e723e688bc24d6a14ed2fdfc83c5
devansh01sep <devanshofficial0109@gmail.com>
2026-07-05 11:31:33 +0530

    Search in a Binary Search Tree

---
+++ Week_4/Day_3/SearchinBtree.java
@@ -0,0 +1,19 @@
+package Week_4.Day_3;
+
+import javax.swing.tree.TreeNode;
+
+public class SearchinBtree {
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == val) {
+            return root;
+        } else if (root.val > val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }
+    }
+}
