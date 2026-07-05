f7d9f6ffdc66c32f5a750a1cf122b24d72284de5
devansh01sep <devanshofficial0109@gmail.com>
2026-07-05 11:29:25 +0530

    Lowest Common Ancestor of a BST

---
+++ Week_4/Day_3/CommonAncestorofBST.java
@@ -0,0 +1,19 @@
+package Week_4.Day_3;
+
+import javax.swing.tree.TreeNode;
+
+public class CommonAncestorofBST {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while (root != null) {
+            if (p.val > root.val && q.val > root.val) {
+                root = root.right;
+            } else if (p.val < root.val && q.val < root.val) {
+                root = root.left;
+            } else {
+                return root;
+            }
+        }
+        return null;
+    }
+}
